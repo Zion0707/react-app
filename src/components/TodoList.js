3b9ae621d0fd1e381a1447b61b769295153c1ac1
huangzihao <346445684@qq.com>
2020-04-28 21:03:50 +0800

    change

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+
 class TodoList extends Component{
+    componentDidMount(){
+
+    }
+
+    dels(item){
+        this.props.deletes(item);
+    }
     render(){
+        var {list, message} = this.props;
         return (
-            <div className="page">
-                
+            <div className="todo-list">
+                todolist
+                <ul>
+                    {
+                        list.map((item, index)=>{
+                            return <li key={index} onClick={this.dels.bind(this, item)}>{item}</li>
+                        })
+                    }
+                </ul>
             </div>
         )
     }
 }
+
+export default TodoList;
--- src/views/News/Three.js
+++ src/views/News/Three.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
+import TodoList from '@/components/TodoList';
+
 class Three extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            list:[
+                '电流',
+                '电压',
+                '母排'
+            ],
+            message:'react is cool！'
+        }
+    }
+
+    dels(index){
+        console.log(index)
+    }
+
     render(){
         return (
             <div className="page">
-                33
+                <TodoList deletes={this.dels} list={this.state.list} message={this.state.message}/>
             </div>
         )
     }
