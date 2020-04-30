d11199ccac8f292c575389f8a9733b445c288346
huangzihao <346445684@qq.com>
2020-04-30 11:10:24 +0800

    添加数据持久化

--- src/index.js
+++ src/index.js
@@ -6,9 +6,14 @@ import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
 import store from '@/store/index';
 
+import {persistor} from '@/store/index'
+import {PersistGate} from 'redux-persist/lib/integration/react';
+
 ReactDOM.render(
 	<Provider store={store}>
-		<App />
+		<PersistGate loading={null} persistor={persistor}>
+			<App />
+		</PersistGate>
 	</Provider>,	
   	document.getElementById('root')
 );
--- src/store/index.js
+++ src/store/index.js
@@ -2,5 +2,15 @@
 import { createStore } from 'redux'; //从redux引入createStore方法
 import reducer from '@/store/reducers'; //导入根reducers 
 
-let store = createStore(reducer);//传入reducer
+import {persistStore, persistReducer} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const persistConfig = {
+    key: 'root',
+    storage: storage,
+};
+const myPersistReducer = persistReducer(persistConfig, reducer)
+const store = createStore(myPersistReducer)
+
+export const persistor = persistStore(store)
 export default store;
