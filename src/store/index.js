89776567752eadf72934cd65a6199a02ba1f7ec7
huangzihao <346445684@qq.com>
2020-04-30 11:16:21 +0800

    添加数据持久化

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,13 @@ import { createStore } from 'redux'; //从redux引入createStore方法
 import reducer from '@/store/reducers'; //导入根reducers 
 
 import {persistStore, persistReducer} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import session from 'redux-persist/lib/storage/session'; //存储方式 session ， storage， cookie
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 const persistConfig = {
     key: 'root',
-    storage: storage,
+    storage: session,
+    stateReconciler: autoMergeLevel2 // 查看 'Merge Process' 部分的具体情况
 };
 const myPersistReducer = persistReducer(persistConfig, reducer)
 const store = createStore(myPersistReducer)
