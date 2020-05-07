4f67bacb282cac36b93e54298b8073b0eb8f13da
huangzihao <346445684@qq.com>
2020-05-07 11:29:31 +0800

    change

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 class Home extends Component {
+    clickMyBtn(){
+        for(var i=0;i<3;i++){
+            
+            console.log(i);
+        }
+    }
     render() {
 		return (
             <div className="page">
                 home
-       
+                <button onClick={this.clickMyBtn.bind(this)}>点击</button>
             </div>
         )
     }
