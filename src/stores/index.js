180f45ee6fb5c9603ea36185f9a22e471dce8b8d
huangzihao <346445684@qq.com>
2020-05-26 10:15:40 +0800

    refactor(redux): 重构redux

--- src/stores/index.js
+++ src/stores/index.js
@@ -2,7 +2,7 @@
 import { createStore, combineReducers } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import homeReducer from '@/pages/home/reducer';
+import homeReducer from '@/pages/home/store';
 
 const persistConfig = {
     key: 'root',
