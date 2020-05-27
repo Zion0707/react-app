8b050ed6fb619f3ddc98ab6b27a6217b73aebeb0
huangzihao <346445684@qq.com>
2020-05-27 16:59:00 +0800

    mobile转场

--- src/pages/Center/index.js
+++ src/pages/Center/index.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Route, NavLink as Link } from 'react-router-dom';
+import { Route, NavLink as Link, Switch } from 'react-router-dom';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 /**
  * 注意：
- * 子级不需要 BrowserRouter as Router 设置到最外层，这样就可以避免了到了父级
- * 路由，还显示着子级组件内容！
+ * 子级不需要 BrowserRouter as Router 设置到最外层，这样就可以避免了父级路由，还显示着子级组件内容！
  */
 class Center extends Component {
     constructor(props) {
@@ -29,21 +29,46 @@ class Center extends Component {
                             );
                         })}
                     </ul>
-                    {routes.map((route, key) => {
-                        return (
-                            <Route
-                                key={key}
-                                exact={route.exact}
-                                path={route.path}
-                                render={(props) => (
-                                    <route.component
-                                        {...props}
-                                        routes={route.children}
-                                    />
-                                )}
-                            />
-                        );
-                    })}
+                    <TransitionGroup
+                        style={{ position: 'releative' }}
+                        childFactory={(child) =>
+                            React.cloneElement(child, {
+                                classNames:
+                                    this.props.history.action === 'PUSH'
+                                        ? 'page-push'
+                                        : 'page-pop',
+                            })
+                        }
+                    >
+                        <CSSTransition
+                            key={this.props.location.pathname}
+                            timeout={500}
+                            classNames={
+                                this.props.history.action === 'PUSH'
+                                    ? 'page-push'
+                                    : 'page-pop'
+                            }
+                        >
+                            <Switch location={this.props.location}>
+                                {routes.map((route, key) => {
+                                    return (
+                                        <Route
+                                            key={key}
+                                            exact={route.exact}
+                                            path={route.path}
+                                            component={route.component}
+                                            // render={(props) => (
+                                            //     <route.component
+                                            //         {...props}
+                                            //         routes={route.children}
+                                            //     />
+                                            // )}
+                                        />
+                                    );
+                                })}
+                            </Switch>
+                        </CSSTransition>
+                    </TransitionGroup>
                 </div>
             </>
         );
